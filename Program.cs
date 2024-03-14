3fe86883ca5ff817c90003bc16bb7fb6ac4091be
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-03-14 17:52:20 -0300

    resolvido primeira parte com lógica estrutural.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemaAlmentoSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome do funcionário: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o salário bruto do funcionário: ");
+            double salarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite a tacha de imposto");
+            double impostop = double.Parse(Console.ReadLine());
+
+            double imposto = salarioBruto * (impostop / 100);
+            double salarioLiquido = salarioBruto - imposto;
+
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
+
+            Console.Write("Digite a porcentagem de aumento salarial: ");
+            double aumentoPorcentagem = double.Parse(Console.ReadLine());
+            double aumentoValor = salarioBruto * (aumentoPorcentagem / 100);
+            double novoSalarioBruto = salarioBruto + aumentoValor;
+            double novoImposto = novoSalarioBruto * (impostop / 100);
+            double novoSalarioLiquido = novoSalarioBruto - novoImposto;
+
+            Console.WriteLine("dados atualizados");
+            Console.WriteLine($"Novo Salário Líquido: R$ {novoSalarioLiquido:F2}");
+            Console.ReadKey();
+        }
+    }
+}
